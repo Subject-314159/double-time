d31b4c8ee0dbc587529704d030271d27ccc49649
Subject-314159 <168562337+Subject-314159@users.noreply.github.com>
2026-02-23 22:32:07 +0100

    Initial commit

---
+++ settings.lua
@@ -0,0 +1,16 @@
+data:extend({{
+    type = "bool-setting",
+    name = "dt-double-speed",
+    setting_type = "startup",
+    default_value = true
+}, {
+    type = "bool-setting",
+    name = "dt-double-productivity",
+    setting_type = "startup",
+    default_value = true
+}, {
+    type = "bool-setting",
+    name = "dt-double-range",
+    setting_type = "startup",
+    default_value = true
+}})
